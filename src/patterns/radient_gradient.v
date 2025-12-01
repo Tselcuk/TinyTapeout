2df38cd69bc3bd2335828a36f54088c98bcbca64
JoshuaLeeZhang <jlzhang@uwaterloo.ca>
2025-12-01 14:33:43 -0500

    refactor: adjust base radius calculation

--- src/patterns/radient_gradient.v
+++ src/patterns/radient_gradient.v
@@ -18,18 +18,18 @@ module radient_gradient (
     wire [9:0] dy = (y < 240) ? (240 - y) : (y - 240);
     wire [9:0] manhattan_distance = dx + dy;
 
-    // This expands the pattern outwards
+    // This expands the pattern outwards by changing the base radius
     // Slow down the expansion by 8x since we divide by 8
-    wire [9:0] base_radius = 10'd30 + {2'b0, frame_counter[9:3]};
+    wire [9:0] base_radius = {3'b0, frame_counter[9:3]};
 
     always @(*) begin
         rgb = 6'b000001; // NAVY_EDGE
 
-        if      (manhattan_distance <= base_radius + 0*24) rgb = 6'b101101; // MAGENTA_CORE
-        else if (manhattan_distance <= base_radius + 1*24) rgb = 6'b101100; // MAGENTA_GLOW
-        else if (manhattan_distance <= base_radius + 2*24) rgb = 6'b101000; // MAGENTA_INNER_RING
-        else if (manhattan_distance <= base_radius + 3*24) rgb = 6'b001100; // MAGENTA_OUTER_RING
-        else if (manhattan_distance <= base_radius + 4*24) rgb = 6'b001000; // BLUE_HALO
+        if      (manhattan_distance <= base_radius + 1*24) rgb = 6'b101101; // MAGENTA_CORE
+        else if (manhattan_distance <= base_radius + 2*24) rgb = 6'b101100; // MAGENTA_GLOW
+        else if (manhattan_distance <= base_radius + 3*24) rgb = 6'b101000; // MAGENTA_INNER_RING
+        else if (manhattan_distance <= base_radius + 4*24) rgb = 6'b001100; // MAGENTA_OUTER_RING
+        else if (manhattan_distance <= base_radius + 5*24) rgb = 6'b001000; // BLUE_HALO
     end
 
 endmodule
