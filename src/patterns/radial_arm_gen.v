97b52f6cba0bba2723b0c3f9b8e9b8821ec11261
JoshuaLeeZhang <jlzhang@uwaterloo.ca>
2025-11-30 21:51:46 -0500

    Fix verilog

--- src/patterns/radial_arm_gen.v
+++ src/patterns/radial_arm_gen.v
@@ -1,5 +1,3 @@
-// Originally meant to be a rotating spiral pattern, but calculating exact radius takes too many bits
-// Instead, this splits the screen into 8 angle sectors, shooting out chunks of color that expand outward
 module radial_arm_gen (
     input wire clk,
     input wire rst,
@@ -16,20 +14,25 @@ module radial_arm_gen (
         else if (next_frame) rot_accum <= rot_accum + {5'b0, step_size};
     end
 
+    // Slows down the rotation speed by 4x since we divide by 4
+    // Speed controller doesn't help as even in low speeds, without divison, the pattern is too fast
     wire [5:0] rotation_offset = rot_accum[7:2];
 
     wire [9:0] dx = (x < 320) ? (320 - x) : (x - 320);
     wire [9:0] dy = (y < 240) ? (240 - y) : (y - 240);
-    wire [9:0] radius = dx + dy; // Gets absolute manhattan distance from center of screen
+    // Rational: We are not using manhattan_distance[3:0], but those bits are necessary to properly compute manhattan_distance[9:4]
+    /* verilator lint_off UNUSEDSIGNAL */
+    wire [9:0] manhattan_distance = dx + dy; // Gets absolute manhattan distance from center of screen
+    /* verilator lint_on UNUSEDSIGNAL */
 
     // Constructs a 6-bit angle value by using the x and y coordinates to create a 3-bit angle sector and adding the rotation offset
     wire [5:0] angle = {(x >= 320), (y >= 240), (dx > dy), 3'b0} + rotation_offset;
 
-    // This is what creates the arms, (angle - radius) forms the desired pattern
-    // If radius[9:4] > angle, by 2s complement, radial_phase[6] will be 1
+    // This is what creates the arms, (angle - manhattan_distance) forms the desired pattern
+    // If manhattan_distance[9:4] > angle, by 2s complement, radial_phase[6] will be 1
     // Rational: We are not using radial_phase[2:0], but as that is only three bits, we can safely ignore the warning
     /* verilator lint_off UNUSEDSIGNAL */
-    wire [6:0] radial_phase = {1'b0, angle} - {1'b0, radius[9:4]};
+    wire [6:0] radial_phase = {1'b0, angle} - {1'b0, manhattan_distance[9:4]};
     /* verilator lint_on UNUSEDSIGNAL */
 
     wire [5:0] arm_color =
